d97cb084b812932cc14f2086d553ca9ed842726a
didiguillen <daguillen@miners.utep.edu>
2023-06-27 18:15:46 -0600

    still working on tokenize

--- src/chars.c
+++ src/chars.c
@@ -23,17 +23,17 @@ char *copy_str(char *inStr, short len)
   char *p = malloc(sizeof(char)*(len+1));
   for(int i = 0; i < len; i++){
     *p = *inStr;
-    printf("%c ", *p);
+    //printf("%c ", *p);
     p++;
     inStr++;
   }
-  printf("%c ", *p);
+  //printf("%c ", *p);
   if(*p != '\0'){
     *(p+1) = '\0';
     p++;
   }
   p = p - len;
-  printf("%c ", *p);
+  //printf("%c ", *p);
   return p;
 }
 
@@ -76,7 +76,7 @@ int count_tokens(char *str)
       continue;
     }
   }
-  printf("%d\n", num_tokens);
+  printf("num tokens: %d\n", num_tokens);
   return num_tokens;
 }
 
@@ -84,20 +84,23 @@ char **tokenize(char *str)
 {
   int num_tokens = count_tokens(str);
   if(num_tokens){
-    char **p = malloc((num_tokens+1)*sizeof(char));
+    char **p = malloc((num_tokens+1)*sizeof(char*));
     short len = token_terminator(str) - token_start(str);
     char *token = copy_str(str, len);
     *p = token;
     p++;
     str = str + len;
     token = token_terminator(str);
-    while(p < (num_tokens+1)){
+    while(p < (num_tokens)){
       len = token_terminator(token) - token_start(token);
       token = copy_str(str, len);
       *p = token;
       p++;
       str = str + len;
     }
+    char *end_str = malloc(sizeof(char));
+    *end_str = '\0';
+    *p = end_str;
     return p;
   }
   else
